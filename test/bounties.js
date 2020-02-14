3ecede38bb9ff734bd6bf00b230f8099f6093952
peter <psvehla@hotmail.com>
2020-02-14 19:00:46 +1100

    Added fulfilment tests.

--- test/bounties.js
+++ test/bounties.js
@@ -88,4 +88,54 @@ contract('Bounties', function(accounts) {
     await increaseTimeInSeconds((dayInSeconds * 2) + 1);
     assertRevert(bountiesInstance.fulfilBounty(0, "data", {from: accounts[1]}), "Fulfilment accepted after deadline.");
   });
+
+  it("Should allow an issuer to accept the fulfilment of a bounty.", async () => {
+    let time = await getCurrentTime();
+
+    await bountiesInstance.issueBounty("data", time + (dayInSeconds * 2), {from: accounts[0], value: 500000000000});
+    await bountiesInstance.fulfilBounty(0, "data", {from: accounts[1]});
+    let tx = await bountiesInstance.acceptFulfilment(0, 0, {from: accounts[0]});
+
+    assert.strictEqual(tx.receipt.logs.length, 1, "acceptFulfilment() call did not log 1 event");
+    assert.strictEqual(tx.logs.length, 1, "acceptFulfilment() call did not log 1 event");
+
+    const logBountyIssued = tx.logs[0];
+
+    assert.strictEqual(logBountyIssued.event, "FulfilmentAccepted", "acceptFulfilment() call did not log event FulfilmentAccepted.");
+    assert.strictEqual(logBountyIssued.args.bountyId.toNumber(), 0, "FulfilmentAccepted event logged did not have expected bountyId.");
+    assert.strictEqual(logBountyIssued.args.issuer, accounts[0], "FulfilmentAccepted event logged did not have expected issuer.");
+    assert.strictEqual(logBountyIssued.args.fulfiler, accounts[1], "FulfilmentAccepted event logged did not have expected fulfiler.");
+    assert.strictEqual(logBountyIssued.args.fulfilmentId.toNumber(), 0, "FulfilmentAccepted event logged did not have expected fulfilmentId.");
+    assert.strictEqual(logBountyIssued.args.amount.toNumber(), 500000000000, "BountyIssued event logged did not have expected amount.");
+  });
+
+  it("Should not let an issuer accept a non existent fulfilment.", async () => {
+    let time = await getCurrentTime();
+    await bountiesInstance.issueBounty("data", time + (dayInSeconds * 2), {from: accounts[0], value: 500000000000});
+    await bountiesInstance.fulfilBounty(0, "data", {from: accounts[1]});
+    assertRevert(bountiesInstance.acceptFulfilment(0, 1, {from: accounts[0]}), "Allowed the acceptance of a non existent fulfilment.");
+  });
+
+  it("Should only let an issuer accept a fulfilment.", async () => {
+    let time = await getCurrentTime();
+    await bountiesInstance.issueBounty("data", time + (dayInSeconds * 2), {from: accounts[0], value: 500000000000});
+    await bountiesInstance.fulfilBounty(0, "data", {from: accounts[1]});
+    assertRevert(bountiesInstance.acceptFulfilment(0, 0, {from: accounts[1]}), "Someone other than the issuer was allowed to accept the fulfilment.");
+  });
+
+  it("Should not allow the acceptance of an accepted fulfilment.", async () => {
+    let time = await getCurrentTime();
+    await bountiesInstance.issueBounty("data", time + (dayInSeconds * 2), {from: accounts[0], value: 500000000000});
+    await bountiesInstance.fulfilBounty(0, "data", {from: accounts[1]});
+    await bountiesInstance.acceptFulfilment(0, 0, {from: accounts[0]});
+    assertRevert(bountiesInstance.acceptFulfilment(0, 0, {from: accounts[0]}), "Allowed a double fulfilment.");
+  });
+
+  it("Should not allow the acceptance of a cancelled fulfilment.", async () => {
+    let time = await getCurrentTime();
+    await bountiesInstance.issueBounty("data", time + (dayInSeconds * 2), {from: accounts[0], value: 500000000000});
+    await bountiesInstance.fulfilBounty(0, "data", {from: accounts[1]});
+    await bountiesInstance.cancelBounty(0, {from: accounts[0]});
+    assertRevert(bountiesInstance.acceptFulfilment(0, 0, {from: accounts[0]}), "Allowed acceptance of a cancelled fulfilment.");
+  });
 });
